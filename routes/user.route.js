b3ccc1cfa695eb429f22982dbe62ad6402791736
Quddus Salam <quddussalam555@gmail.com>
2023-10-27 21:49:05 +0700

    Minor change to how server would start

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.send("TODO: Create user route.");
+  res.status(418).json({ message: "Feature haven't been implemented!" });
 });
 
 export default router;
--- server.js
+++ server.js
@@ -10,37 +10,49 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-
 import chamberRoute from "./routes/chamber.route.js";
 import userRouter from "./routes/user.route.js";
 
 const app = express();
-dotenv.config();
-
-const PORT = process.env.SERVER_PORT;
-const DATABASE_URI = process.env.MONGODB_DATABASE_URL;
-
-// Connecting to MongoDB.
-async function main() {
-  try {
-    await mongoose.connect(DATABASE_URI);
-  } catch (error) {
-    throw new Error(`An error occurred: ${error}`);
-  }
-}
-
-main();
-
-mongoose.connection
-  .on("open", () => console.log("Database connected!"))
-  .on("error", (err) => console.error(err.message));
-
 app.use(cors());
 app.use(express.json());
 
 app.use("/chambers", chamberRoute);
 app.use("/users", userRouter);
 
-app.listen(PORT, () => {
-  console.log(`Listening to http://localhost:${PORT}/`);
-});
+dotenv.config();
+const DATABASE_URI = process.env.MONGODB_DATABASE_URL;
+const PORT = 5000;
+
+// Connecting to MongoDB then establishing a connection.
+async function main(url, port) {
+  try {
+    connectMongoDB(url);
+
+    await new Promise((resolve, reject) => {
+      mongoose.connection.on("open", resolve).on("error", reject);
+    });
+
+    startServer(port);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+async function connectMongoDB(url) {
+  try {
+    await mongoose.connect(url);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+function startServer(port) {
+  app.listen(port, () => {
+    console.log(
+      `Database connected! - Server connection has been established on PORT: ${port}`
+    );
+  });
+}
+
+main(DATABASE_URI, PORT);
